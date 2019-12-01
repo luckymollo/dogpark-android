5f7f86daeb7c14290f57316e1a75de40846d7dab
Abhishek Vadnerkar <avadnerkar@gmail.com>
2019-12-01 17:40:47 -0500

    Test commit

--- client/src/main/java/com/luckymollo/dogpark/client/test/TestViewModel.kt
+++ client/src/main/java/com/luckymollo/dogpark/client/test/TestViewModel.kt
@@ -3,10 +3,10 @@ package com.luckymollo.dogpark.client.test
 import com.luckymollo.dogpark.client.data.Repository
 import com.luckymollo.dogpark.core.base.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class TestViewModel(repository: Repository) : MviViewModel<Repository, State, BaseAction, BaseResult, BaseEvent>(repository, State()) {
-    override fun handle(action: BaseAction): Flow<BaseResult> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun handle(action: BaseAction): Flow<BaseResult> = flow {
     }
 
     override fun reduce(result: BaseResult): State {
